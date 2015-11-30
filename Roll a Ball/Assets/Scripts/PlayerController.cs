a4e6f6a9cbebecd38bfaad59c715a044a69181a7
IvoHefti <hefti.ivo@gmail.com>
2015-11-30 13:03:20 +0100

    Roll a Ball en Space Shooter projects

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 0;
+	public Text countText;
+	public Text winText;
+	
+    private Rigidbody rb;
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText();
+		winText.text = "";
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+    }
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive(false);
+			count += 1;
+			SetCountText();
+		}
+	}
+	
+	void SetCountText(){
+		countText.text = "Count: " + count.ToString();
+		if(count >= 11){
+			winText.text = "You Win!";
+		}
+	}
+}
